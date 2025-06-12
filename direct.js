fba6f210dd59988a44237930ff4ef7e4cc546505
mbelzzz <haiworld@tuta.io>
2025-06-13 00:08:10 +0700

    Create direct.js

---
+++ direct.js
@@ -0,0 +1,12 @@
+var target = [
+"https://www.profitableratecpm.com/aw2prxvd?key=ffee8b30495403f4d74c09fb339f4bee",
+];
+var maxclick=1;
+function tampilkanUrl(){
+  if(maxclick<=0)
+    return;
+  maxclick--;
+  var url = target[Math.floor(Math.random()*target.length)];
+  window.open(url);
+}
+document.body.addEventListener("click",tampilkanUrl);
